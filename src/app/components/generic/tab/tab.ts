f55c7976e9f13b4aa8a429924fb72f9689a2c140
Ronan Brett <Ronan.Brett@theaa.ie>
2016-08-23 17:18:24 +0100

    Update Tabs to new style

--- src/app/components/generic/tab/tab.ts
+++ src/app/components/generic/tab/tab.ts
@@ -1,59 +1,65 @@
-import { Component, Attribute, OnInit, Optional, SkipSelf, Host } from '@angular/core';
-import {isPresent} from '@angular/platform-browser/src/facade/lang';
-import {TabsDispatcher} from './tab_dispatcher';
-import {TabGroupComponent} from './tab_group';
+import { Component, OnInit, Input, Optional, SkipSelf, Host, HostBinding } from '@angular/core';
+import { isPresent } from '@angular/platform-browser/src/facade/lang';
+import { TabsDispatcher } from './tab_dispatcher';
+import { TabGroupComponent } from './tab_group';
 
 @Component({
 	selector: 'c-tab',
-	inputs: ['id', 'name', 'selected', 'disabled', 'value'],
-	templateUrl: './tab.html',
-	host: {
-		'[id]': 'id',
-		'[tabindex]': 'tabindex',
-		'[class.selected]': 'isSelected',
-		'[attr.aria-disabled]': 'disabled'
-	}
+	templateUrl: './tab.html'
 })
 export class TabComponent implements OnInit {
-	isSelected: boolean = false;
-	selected: boolean;
-	public id: string;
-	public name: string;
-	public value: string;
-	private _disabled: boolean = false;
+
+	@HostBinding('attr.id') get _attrId() { return this.id; };
+	@HostBinding('attr.tabindex') get attrTabIndex() { return this._tabIndex; };
+	@HostBinding('class.selected') get attrSelected() { return this._isSelected; };
+	@HostBinding('class.aria-disabled') get attrDisabled() { return this._isDisabled; };
+
+	@Input('id') set id(v) { this._id = v ? v : 'tab'; };
+	@Input('name') name: string;
+	@Input('tabIndex') set tabIndex(v) { this._tabIndex = v ? v : -1; };
+	@Input('value') value: string;
+
+	@Input('disabled')
+	set disabled(v: boolean) { this._isDisabled = v ? v : false; }
+
+	@Input('selected') set isSelected(v) {
+		this._isSelected = v ? v : false;
+	}
+
+	get isSelected() {
+		return this._isSelected;
+	}
+
+	private _isSelected: boolean;
+	private _id;
+	private _tabIndex: number;
+	private _isDisabled: boolean = false;
+
 
 	constructor(
 		@Optional() @SkipSelf() @Host() public tabGroup: TabGroupComponent,
-		@Attribute('id') id: string,
-		@Attribute('selected') selected: boolean,
-		@Attribute('tabindex') tabindex: string,
 		public tabsDispatcher: TabsDispatcher
-	) {
-		tabsDispatcher.listen((name) => {
-			if (name === this.name) {
-				this.isSelected = false;
-			}
-		});
-		this.name = tabGroup.getName();
-		this.tabGroup.register(this);
-
-	}
+	) { }
 
 	ngOnInit() {
-		if (isPresent(this.selected)) {
+		this.tabsDispatcher.listen((name) => {
+			if (name === this.name) {
+				this._isSelected = false;
+			}
+		});
+		this.name = this.tabGroup.getName();
+		this.tabGroup.register(this);
+		if (isPresent(this._isSelected)) {
 			this.tabGroup.select(this);
 		}
 	}
 
 	isDisabled(): boolean {
-		return this._disabled || (isPresent(this.tabGroup) && this.tabGroup.disabled);
+		return this._isDisabled || (isPresent(this.tabGroup) && this.tabGroup.disabled);
 	}
 
 	get disabled(): boolean {
 		return this.isDisabled();
 	}
 
-	set disabled(value: boolean) {
-		this._disabled = isPresent(value) && value !== false;
-	}
 }
