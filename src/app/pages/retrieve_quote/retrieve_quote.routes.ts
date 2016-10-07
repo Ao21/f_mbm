6d10115743ebf8565ce2801322ef224422df51cd
Ronan Brett <Ronan.Brett@theaa.ie>
2016-10-07 10:44:19 +0100

    Add Default Retrieve Quote Route

--- src/app/pages/retrieve_quote/retrieve_quote.routes.ts
+++ src/app/pages/retrieve_quote/retrieve_quote.routes.ts
@@ -4,5 +4,6 @@ import { MembershipRetrieveQuoteComponent } from './retrieve_quote.component';
 export const RetrieveQuoteRoutes: Routes = [
 	{ path: 'retrieve_quote/myaa/:myaa', component: MembershipRetrieveQuoteComponent},
 	{ path: 'retrieve_quote/:ref', component: MembershipRetrieveQuoteComponent},
-	{ path: 'retrieve_quote/:ref/:dob', component: MembershipRetrieveQuoteComponent}
+	{ path: 'retrieve_quote/:ref/:dob', component: MembershipRetrieveQuoteComponent},
+	{ path: 'retrieve_quote', component: MembershipRetrieveQuoteComponent},
 ];
